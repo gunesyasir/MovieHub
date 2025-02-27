2ad99c5a6f5b41581cb910e1f0f41a9f28940f58
gunesyasir <myasirgunes@gmail.com>
2025-02-28 01:10:51 +0300

    first commit

---
+++ MovieHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHub
+//
+//  Created by Yasir Gunes on 28.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieHubTests/MovieHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubTests.swift
+//  MovieHubTests
+//
+//  Created by Yasir Gunes on 28.02.2025.
+//
+
+import Testing
+@testable import MovieHub
+
+struct MovieHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
